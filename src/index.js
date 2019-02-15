afaa1f63b3bd01370951c2debd8990ecab7dc930
fajing.wang <Fajing.Wang@b-and-qchina.com>
2019-02-15 22:40:45 +0800

    修改

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
  * Created by wangfajing on 2018/12/24.
  */
 import ReactDom from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import App from './App';
 
